4b48f4add26cfe54c052686d69a5a3d0da39fb5a
ubermanu <manu@paca.happy-dev.fr>
2023-03-13 13:29:18 +0100

    add svelte plugin ass peer dep

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,19 @@
-module.exports = {
+const tryRequire = require('try-require')
+const svelte = tryRequire.resolve('prettier-plugin-svelte')
+
+const config = {
   trailingComma: 'es5',
   tabWidth: 2,
   semi: false,
   singleQuote: true,
+  plugins: [],
+  pluginSearchDirs: ['.'],
+  overrides: [],
 }
+
+if (svelte) {
+  config.plugins.push(svelte)
+  config.overrides.push({ files: '*.svelte', options: { parser: 'svelte' } })
+}
+
+module.exports = config
